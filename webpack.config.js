63eb0f38b223b338b96acf3138d3eb18ed4ab12e
Feitan <55014613+JustFeitan@users.noreply.github.com>
2023-05-21 07:26:37 +0300

    Made main webpack and folder setup

---
+++ webpack.config.js
@@ -0,0 +1,104 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+const mode = process.env.NODE_ENV || "production";
+const devMode = mode === "development";
+const target = devMode ? "web" : "browserslist"
+
+const devtool = devMode ? "source-map" : undefined;
+
+const plugins = devMode
+    ?[
+        new ReactRefreshWebpackPlugin()
+    ]
+    : [
+        new MiniCssExtractPlugin({
+            filename: "index[contenthash].css"
+        })
+    ]
+
+module.exports = {
+    mode,
+    devtool,
+    target,
+    entry: [path.resolve(__dirname, 'src', 'index.tsx')],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.[contenthash].js",
+        clean: true,
+        assetModuleFilename: "assets/[hash][ext]"
+    },
+    devServer: {
+        port: 3000,
+        hot: true,
+        open: true,
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html"),
+        }),
+        new CssMinimizerPlugin(),
+        ...plugins
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "resolve-url-loader",
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+                type: devMode ? "asset/resource" : "asset",
+
+            },
+            {
+                test: /\.tsx?$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true,
+                    },
+
+                },
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                }
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".jsx"],
+    },
+    optimization: {
+        minimizer: [
+            new CssMinimizerPlugin()
+        ]
+    },
+    performance: {
+        hints: false
+    }
+}
